3db51d531a521e678fb1730df29b3a59baebe418
Arnaud Duhé <anro010@hotmail.com>
2020-04-27 11:22:46 +0100

    quest working fine

---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Counter() {
+
+    const[count, setCount] = useState();
+
+    function handleInitialCountChange(e) {
+        setCount(Number(e.target.value));
+    }
+    
+console.log(typeof count)
+return (
+    <div>
+    <p>The counter is at : {count} </p>
+    <button onClick={() => setCount(count + 1)}>>
+        +1
+    </button>
+    <button onClick={() => setCount(count - 1)}>>
+        -1
+    </button>
+    <input 
+        value={count}
+        onChange={handleInitialCountChange}>
+        </input>
+    </div>
+);
+}
+
+export default Counter;
